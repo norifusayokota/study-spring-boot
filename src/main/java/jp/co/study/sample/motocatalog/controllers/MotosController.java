3f072d21443e3bc4670428673b676ee2b1787b76
norifusa.yokota <norifusa.yokota@rensa.co.jp>
2025-09-19 12:43:13 +0900

    クエリパラメータを取得して、画面に表示する方法の学習

--- src/main/java/jp/co/study/sample/motocatalog/controllers/MotosController.java
+++ src/main/java/jp/co/study/sample/motocatalog/controllers/MotosController.java
@@ -1,13 +1,18 @@
 package jp.co.study.sample.motocatalog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MotosController {
-    
+
     @RequestMapping("/test")
-    public String test() {
+    public String test(@RequestParam String name, Model model) {
+        // @RequestParamで指定した変数名は、URLのリクエストパラメータ名と一致させる必要がある
+        // model.addAttribute()：Spring BootのコントローラーからHTMLテンプレートにデータを渡すためのメソッド
+        model.addAttribute("name", name);
         return "test";
     }
 }
