2f29b1c42a1fd8dc5d445d99a085dd05c55425b3
norifusa.yokota <norifusa.yokota@rensa.co.jp>
2025-09-29 10:56:29 +0900

    サービスのコメントアウトの追加

--- src/main/java/jp/co/study/sample/motocatalog/sevices/MotosService.java
+++ src/main/java/jp/co/study/sample/motocatalog/sevices/MotosService.java
@@ -15,8 +15,11 @@ import jp.co.study.sample.motocatalog.model.SearchForm;
 public class MotosService {
 
     /*
-     * SpringのDIコンテナ（アプリ起動時にSpringフレームワークが特定のオブジェクトをインスタンス化しており、そのインスタンスを保管してある場所）から、インスタンスを注入するための指定
-     * @Component、@Controller、@Serviceなどのアノテーションがついたクラスはコードのなかでnew()でインスタンス化することはなく、DIの仕組みによって呼び出し元クラスへ注入される
+     * SpringのDIコンテナ（アプリ起動時にSpringフレームワークが特定のオブジェクトをインスタンス化しており、そのインスタンスを保管してある場所）から
+     * 、インスタンスを注入するための指定
+     * 
+     * @Component、@Controller、@Serviceなどのアノテーションがついたクラスはコードのなかでnew()でインスタンス化することはなく、
+     * DIの仕組みによって呼び出し元クラスへ注入される
      */
     @Autowired
     MotorcycleMapper motorcycleMapper;
@@ -28,18 +31,41 @@ public class MotosService {
     @Autowired
     BrandMapper brandMapper;
 
+    /**
+     * 検索条件に従ったバイク情報の一覧を取得する
+     * 
+     * @param condition 検索条件
+     * @return バイクの情報の一覧
+     */
     public List<Motorcycle> getMotorcycleList(SearchForm condition) {
         return motorcycleMapper.selectByCondition(condition);
     }
 
+    /**
+     * バイクの番号（プライマリキー）に従ったバイク情報を取得する
+     * 
+     * @param motorcycleNo バイクの番号（プライマリキー）
+     * @return バイクの情報
+     */
     public Motorcycle getMotorcycle(int motorcycleNo) {
         return motorcycleMapper.selectByMotorcycleNo(motorcycleNo);
     }
 
+    /**
+     * バイクのブランドの一覧を取得する
+     * 
+     * @return バイクのブランドの一覧
+     */
     public List<Brand> getBrand() {
         return brandMapper.selectAll();
     }
 
+    /**
+     * バイク情報を更新する
+     * 
+     * @param motorcycle バイク情報
+     * @return 更新件数
+     */
     public int save(Motorcycle motorcycle) {
         return motorcycleMapper.update(motorcycle);
     }
